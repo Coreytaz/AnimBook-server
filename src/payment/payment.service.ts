7e3a3855708da4b64483e891f3e80ffd743a9176
Coreytaz <coreytaz321@gmail.com>
2023-06-01 14:16:20 +0500

    return axiosref

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,13 +1,11 @@
-import { ForbiddenException, Injectable, Logger } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+
 import { ConfigService } from '@nestjs/config';
 import { MakePaymentDto } from './dto/make-payment.dto';
 import { HttpService } from '@nestjs/axios';
-import { catchError, firstValueFrom } from 'rxjs';
-import { PaymentCheck, PaymentCreate } from './types';
 
 @Injectable()
 export class PaymentService {
-  private readonly logger = new Logger(PaymentService.name);
   constructor(
     private readonly configService: ConfigService,
     private readonly httpService: HttpService,
@@ -15,43 +13,33 @@ export class PaymentService {
 
   async makePayment(dto: MakePaymentDto) {
     try {
-      const { data } = await firstValueFrom(
-        this.httpService
-          .post<PaymentCreate>(
-            'https://api.yookassa.ru/v3/payments',
-            {
-              amount: {
-                value: dto.amount,
-                currency: 'RUB',
-              },
-              capture: true,
-              confirmation: {
-                type: 'redirect',
-                return_url: `${this.configService.get(
-                  'API_FRONT',
-                )}/cart/result/${dto.orderId}`,
-              },
-              description: `Заказ №${dto.orderId}`,
-            },
-            {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-                'Idempotence-Key': Date.now(),
-              },
-              auth: {
-                username: '215073',
-                password: 'test_FevXsgz4-yKXptOaLNzloeqKI79o_7oR7j89hqudydg',
-              },
-            },
-          )
-          .pipe(
-            catchError((error) => {
-              this.logger.error(error.response.data);
-              throw 'An error happened!';
-            }),
-          ),
-      );
+      const { data } = await this.httpService.axiosRef({
+        method: 'POST',
+        url: 'https://api.yookassa.ru/v3/payments',
+        headers: {
+          'Content-Type': 'application/json',
+          'Idempotence-Key': Date.now(),
+        },
+        auth: {
+          username: '215073',
+          password: 'test_FevXsgz4-yKXptOaLNzloeqKI79o_7oR7j89hqudydg',
+        },
+        data: {
+          amount: {
+            value: dto.amount,
+            currency: 'RUB',
+          },
+          capture: true,
+          confirmation: {
+            type: 'redirect',
+            return_url: `${this.configService.get('API_FRONT')}/cart/result/${
+              dto.orderId
+            }`,
+          },
+          description: `Заказ №${dto.orderId}`,
+        },
+      });
+
       return data;
     } catch (error) {
       throw new ForbiddenException(error);
@@ -60,25 +48,15 @@ export class PaymentService {
 
   async checkPayment(paymentId: string) {
     try {
-      const { data } = await firstValueFrom(
-        this.httpService
-          .get<PaymentCheck>(
-            `https://api.yookassa.ru/v3/payments/${paymentId}`,
-            {
-              method: 'GET',
-              auth: {
-                username: '215073',
-                password: 'test_FevXsgz4-yKXptOaLNzloeqKI79o_7oR7j89hqudydg',
-              },
-            },
-          )
-          .pipe(
-            catchError((error) => {
-              this.logger.error(error.response.data);
-              throw 'An error happened!';
-            }),
-          ),
-      );
+      const { data } = await this.httpService.axiosRef({
+        method: 'GET',
+        url: `https://api.yookassa.ru/v3/payments/${paymentId}`,
+        auth: {
+          username: '215073',
+          password: 'test_FevXsgz4-yKXptOaLNzloeqKI79o_7oR7j89hqudydg',
+        },
+      });
+
       return data;
     } catch (error) {
       throw new ForbiddenException(error);
